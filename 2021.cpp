e32204cafe86b5ce9f97addd07ef42333fa821b6
liuwei <403905676@qq.com>
2021-06-07 17:38:40 +0800

    sb me

--- 2021.cpp
+++ 2021.cpp
@@ -6,6 +6,22 @@
 #include <algorithm>
 using namespace std;
 
+//暂时把GLSL源代码写死在C风格字符串中
+//顶点着色器
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//片段着色器
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -47,6 +63,63 @@ int main()
 
     //注册调整窗口大小回调函数
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+    //使用glGenBuffer生成VBO对象
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    //把新建的缓冲绑定到顶点缓冲对象缓冲类型GL_ARRAY_BUFFER上
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //把用户定义的数据复制到当前绑定缓冲的函数
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //创建一个顶点着色器对象
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //编译着色器
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    //着色器编译信息输出
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //创建编译片段着色器对象
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+
+    //创建着色器程序对象
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    //将之前编译的着色器附加到程序上，然后链接他们
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    //链接信息输出
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    }
+    //调用着色器程序对象
+    glUseProgram(shaderProgram);
+    //将着色器对象链接到着色器程序对象后可以将着色器对象删除
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
 
     while (!glfwWindowShouldClose(window))
     {
